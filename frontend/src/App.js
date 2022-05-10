4fea5304ac3fb0928408e4c88f490c3adb6020d9
Vignesh Uthrapathy <vigneshuthra@gmail.com>
2022-05-10 15:04:23 +0100

    intial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+    <header>
+      <a href='/'> Shoppy</a>
+    </header>
+    <main>
+      List of objects
+    </main>
+    </div>
+  );
+}
+
+export default App;
